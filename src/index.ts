f36dd733bfc394e924000b802c2754351815043d
Anders Sjöberg <anders.sjoberg.97@gmail.com>
2023-07-05 07:39:20 +0200

    fix: exported BorderPatrolException

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,4 @@ export { createBorder } from "./utility";
 export { UseBorder } from "./border-patrol.decorators";
 export { extendApi as extendBorder } from "@anatine/zod-openapi";
 export { BorderPatrolModule } from "./border-patrol.module";
+export { BorderPatrolException } from "./border-patrol.exception";
